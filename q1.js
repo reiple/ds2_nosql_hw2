93bcb28daf5d1b8ad4860df0bb78e769cc29f9bc
reiple <trsprs@gmail.com>
2023-07-26 19:01:52 +0900

    Q1에 대한 조건 작성 중

--- q1.js
+++ q1.js
@@ -1,5 +1,13 @@
+
+// 격자를 0.1 단위로 하고, 각 격자 내에 있는 사용자 수를 구해야 함
 var result = db.gps.aggregate([
-    // TODO: 
+    {$match: {time: {$gte: input_date}}},
+    // {$project: 
+    //     {loc: {$divide: [{$subtract: ["$coordinates[0]", {$mod: ["$coordinates[1]", 0.1]}]}, 0.1]}, 
+    //     lat: {$divide: [{$subtract: ["$$coordinates[1]", {$mod: ["$coordinates[1]", 0.1]}]}, 0.1]}}
+    // },
+    {$group: {_id: {loc: "$loc", lat: "$lat"}, user_count: {$sum: 1}}},
+    {$sort: {_id: 1}}
  ]);
  
  result.forEach(function(doc) {
